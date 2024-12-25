bc90e9965e4b4b92029e572fa8ca3b4a5c27defd
David Erel <david@piiano.com>
2024-12-25 17:42:03 +0200

    supporting access reson as optional

--- sdk/client/src/main/java/com/piiano/vault/client/TokenClient.java
+++ sdk/client/src/main/java/com/piiano/vault/client/TokenClient.java
@@ -25,14 +25,10 @@ public class TokenClient {
     }
 
     public List<TokenValue> tokenize(TokenizeParams tokenizeParams) throws ApiException {
-
-        if (tokenizeParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         return this.tokensApi.tokenize(
                 tokenizeParams.getCollection(),
-                tokenizeParams.getAccessReason().getReason(),
                 tokenizeParams.getTokenizeRequest())
+                .reason(tokenizeParams.getAccessReason().getReason())
                 .adhocReason(tokenizeParams.getAccessReason().getAdhocReason())
                 .expirationSecs(tokenizeParams.getExpirationSecs())
                 .customAudit(tokenizeParams.getCustomAudit())
@@ -43,13 +39,9 @@ public class TokenClient {
     }
 
     public List<DetokenizedToken> detokenize(DetokenizeParams detokenizeParams) throws ApiException {
-
-        if (detokenizeParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         return this.tokensApi.detokenize(
-                detokenizeParams.getCollection(),
-                detokenizeParams.getAccessReason().getReason())
+                detokenizeParams.getCollection())
+                .reason(detokenizeParams.getAccessReason().getReason())
                 .adhocReason(detokenizeParams.getAccessReason().getAdhocReason())
                 .tokenIds(detokenizeParams.getTokenQuery().getTokenIds())
                 .objectIds(detokenizeParams.getTokenQuery().getObjectIds())
@@ -63,10 +55,6 @@ public class TokenClient {
     }
 
     public List<TokenMetadata> searchTokens(SearchTokensParams searchTokensParams) throws ApiException {
-
-        if (searchTokensParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         QueryToken queryToken = new QueryToken();
         queryToken.setTokenIds(searchTokensParams.getTokenQuery().getTokenIds());
         queryToken.setObjectIds(searchTokensParams.getTokenQuery().getObjectIds());
@@ -74,8 +62,8 @@ public class TokenClient {
 
         return this.tokensApi.searchTokens(
                 searchTokensParams.getCollection(),
-                searchTokensParams.getAccessReason().getReason(),
                 queryToken)
+                .reason(searchTokensParams.getAccessReason().getReason())
                 .adhocReason(searchTokensParams.getAccessReason().getAdhocReason())
                 .options(searchTokensParams.getOptions())
                 .customAudit(searchTokensParams.getCustomAudit())
@@ -85,14 +73,10 @@ public class TokenClient {
     }
 
     public void updateTokens(UpdateTokensParams updateTokensParams) throws ApiException {
-
-        if (updateTokensParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         this.tokensApi.updateTokens(
                 updateTokensParams.getCollection(),
-                updateTokensParams.getAccessReason().getReason(),
                 updateTokensParams.getUpdateTokenRequest())
+                .reason(updateTokensParams.getAccessReason().getReason())
                 .adhocReason(updateTokensParams.getAccessReason().getAdhocReason())
                 .tokenIds(updateTokensParams.getTokenQuery().getTokenIds())
                 .objectIds(updateTokensParams.getTokenQuery().getObjectIds())
@@ -105,13 +89,9 @@ public class TokenClient {
     }
 
     public void deleteTokens(DeleteTokensParams deleteTokensParams) throws ApiException {
-
-        if (deleteTokensParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         this.tokensApi.deleteTokens(
-                deleteTokensParams.getCollection(),
-                deleteTokensParams.getAccessReason().getReason())
+                deleteTokensParams.getCollection())
+                .reason(deleteTokensParams.getAccessReason().getReason())
                 .adhocReason(deleteTokensParams.getAccessReason().getAdhocReason())
                 .tokenIds(deleteTokensParams.getTokenQuery().getTokenIds())
                 .objectIds(deleteTokensParams.getTokenQuery().getObjectIds())
@@ -125,14 +105,10 @@ public class TokenClient {
     }
 
     public Map<String, String> rotateTokens(RotateTokensParams rotateTokensParams) throws ApiException {
-
-        if (rotateTokensParams.getAccessReason() == null) {
-            throw new ApiException("Access reason is required");
-        }
         return this.tokensApi.rotateTokens(
                 rotateTokensParams.getTokenIds(),
-                rotateTokensParams.getCollection(),
-                rotateTokensParams.getAccessReason().getReason())
+                rotateTokensParams.getCollection())
+                .reason(rotateTokensParams.getAccessReason().getReason())
                 .adhocReason(rotateTokensParams.getAccessReason().getAdhocReason())
                 .customAudit(rotateTokensParams.getCustomAudit())
                 .reloadCache(rotateTokensParams.isReloadCache())
